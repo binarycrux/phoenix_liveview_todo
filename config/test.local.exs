5fdc03fecda6bbcd4db6f64d0b28e4c16d832884
BinaryCrux <binarycrux@github.com>
2025-03-04 12:35:00 +0300

    removed config files from tracking

--- config/test.local.exs
+++
@@ -1,9 +0,0 @@
-import Config
-
-config :taskify, Taskify.Repo,
-  username: "postgres",
-  password: "Post@localhost127!",
-  hostname: "localhost",
-  database: "taskify_test#{System.get_env("MIX_TEST_PARTITION")}",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: System.schedulers_online() * 2
